4b46a8be8ed3c5cb80b98b069a640381eab3a190
zhsmnn <zhrstmvva@icloud.com>
2025-02-11 09:03:52 +0600

    .

--- hw_3/todos/urls.py
+++ hw_3/todos/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path, include
-from todos import views
-from .forms import TodoForm
-
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('',views.todo_list, name='todo_list'),
-    path('<int:id>/',views.todo_detail, name='todo_detail'),
-    path('create/',.create_detail, name='todo_create'),
-    path('delete/<int:id>/'views.delete_todo, name='todo_delete'),
+    path('', views.todo_list, name='todo_list'),
+    path('<int:id>/', views.todo_detail, name='todo_detail'),
+    path('create/', views.create_todo, name='todo_create'),
+    path('delete/<int:id>/', views.delete_todo, name='todo_delete'),
 ]
+
